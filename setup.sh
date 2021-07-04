39bd86c074f30eb5e0c33c444c8e33bb14538532
Kevin <kevin@kevin.net.br>
2021-07-04 08:08:18 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,15 +5,15 @@ apk add nano bash bash-completion grep sudo openssh-client ntfs-3g
 nano /etc/apk/repositories
 apk -U upgrade
 
-cho "Preparo da partição /home e add usuário"
+echo "Preparo da partição /home e add usuário"
 echo _____________________________________________________________
 blkid
-echo "informe /home" && echo "ex.: sdc1" && read dhome
-blkid -s UUID -o value /dev/$dhome >/tmp/duuid.txt && read duuid</tmp/duuid.txt && rm /tmp/duuid.txt
-echo "UUID=***"$duuid"***	/home    ext4   defaults    0    2">>/etc/fstab
-mount -t ext4 /dev/dhome /home
+#echo "informe /home" && echo "ex.: sdc1" && read dhome
+#blkid -s UUID -o value /dev/$dhome >/tmp/duuid.txt && read duuid</tmp/duuid.txt && rm /tmp/duuid.txt
+#echo "UUID=***"$duuid"***	/home    ext4   defaults    0    2">>/etc/fstab
+#mount -t ext4 /dev/dhome /home
 clear
-nano /etc/fstab
+#nano /etc/fstab
 echo "Qual seu nome" && read duser
 duser2=`echo "$duser" | tr '[:upper:]' '[:lower:]'`
 
@@ -51,4 +51,5 @@ nmcli device set wlan0 managed yes
 
 cp *.sh /home/$duser2
 chmod +x /home/$duser2/*.sh
+chmod 777 /home/$duser2/*.sh
 
