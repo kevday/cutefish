064b9a07e680c3a0a629f64476f68f45031e3034
Kevin <kevin@kevin.net.br>
2021-12-26 00:19:54 -0300

    add  qt5-qtquickcontrols

--- cutefish_build.sh
+++ cutefish_build.sh
@@ -14,7 +14,7 @@ echo "___________________________________________________________________"
 sleep 3
 sudo apk add nano git pkgconf cmake extra-cmake-modules build-base
 sudo apk add pkgconf-dev qt5-qtquickcontrols2-dev qt5-qtbase-dev qt5-qtx11extras-dev polkit-qt-1-dev pulseaudio-dev kwindowsystem-dev qt5-qttools-dev polkit-dev xcb-util-wm-dev kcoreaddons-dev
-sudo apk add kconfig-dev kdecoration-dev kwin-dev modemmanager-qt-dev networkmanager-qt-dev libdbusmenu-qt-dev libkscreen-dev bluez-qt-dev libcap-dev
+sudo apk add kconfig-dev kdecoration-dev kwin-dev modemmanager-qt-dev networkmanager-qt-dev libdbusmenu-qt-dev libkscreen-dev bluez-qt-dev libcap-dev  qt5-qtquickcontrols
 sudo apk add libqtxdg-dev pulseaudio-dev mpv-dev  libcanberra-gtk2  libcanberra-gtk3  libcanberra-dev xf86-input-libinput-dev xf86-input-synaptics-dev xcb-util-wm-dev libxcbxcb-util-dev
 clear
 echo "___________________________________________________________________"
